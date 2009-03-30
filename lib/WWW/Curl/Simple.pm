b049017e62978efafd08ed636b863868cdf63927
Andreas Marienborg <andreas.marienborg@gmail.com>
2009-03-30 15:21:41 +0200

    Initial import

---
+++ lib/WWW/Curl/Simple.pm
@@ -0,0 +1,85 @@
+package WWW::Curl::Simple;
+
+use Moose;
+
+=head1 NAME
+
+WWW::Curl::Simple - A simpler interface to WWW::Curl
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use WWW::Curl::Simple;
+
+    my $foo = WWW::Curl::Simple->new();
+    ...
+
+=head1 AUTHOR
+
+Andreas Marienborg, C<< <andreas at startsiden.no> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-www-curl-simple at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WWW-Curl-Simple>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc WWW::Curl::Simple
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=WWW-Curl-Simple>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/WWW-Curl-Simple>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/WWW-Curl-Simple>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/WWW-Curl-Simple/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Andreas Marienborg, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of WWW::Curl::Simple
